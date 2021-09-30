e25846113bca81fe2a3aee0109e1546c750739b4
Andrew Moore <andrew.p.moore94@gmail.com>
2021-09-30 11:31:57 +0100

    Windows compatible file paths

--- tests/test_config.py
+++ tests/test_config.py
@@ -9,12 +9,13 @@ def test_cache_home() -> None:
     with tempfile.TemporaryDirectory() as temp_dir:
         home_dir = str(Path.home())
         # Test default home dir
-        assert f'{home_dir}/.cache/pymusas' == config.PYMUSAS_CACHE_HOME
+        
+        assert str(Path(f'{home_dir}', '.cache', 'pymusas')) == config.PYMUSAS_CACHE_HOME
         # Test default home dir when the `XDG_CACHE_HOME` environment variable is set.
-        os.environ["XDG_CACHE_HOME"] = f'{temp_dir}/.test_data'
+        os.environ["XDG_CACHE_HOME"] = str(Path(f'{temp_dir}', '.test_data'))
         reload(config)
-        assert f'{temp_dir}/.test_data/pymusas' == config.PYMUSAS_CACHE_HOME
+        assert str(Path(f'{temp_dir}', '.test_data', 'pymusas')) == config.PYMUSAS_CACHE_HOME
         # Test default home dir when the `PYMUSAS_HOME` environment variable is set.
-        os.environ["PYMUSAS_HOME"] = f'{temp_dir}/.pymusas'
+        os.environ["PYMUSAS_HOME"] = str(Path(f'{temp_dir}', '.pymusas'))
         reload(config)
-        assert f'{temp_dir}/.pymusas' == config.PYMUSAS_CACHE_HOME
+        assert str(Path(f'{temp_dir}', '.pymusas')) == config.PYMUSAS_CACHE_HOME
