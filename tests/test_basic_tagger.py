436ec716eda754f8fd22af98d996e843bbcebb4a
Andrew Moore <andrew.p.moore94@gmail.com>
2021-10-07 22:05:16 +0100

    mypy checking working with python<3.9

--- tests/test_basic_tagger.py
+++ tests/test_basic_tagger.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+from typing import List, Tuple
 
 from pymusas.basic_tagger import RuleBasedTagger
 
@@ -8,8 +9,8 @@ DATA_DIR = Path(__file__, '..', 'data').resolve()
 def test_tag_data() -> None:
     lexicon_path = Path(DATA_DIR, 'lexicon.tsv')
     test_data_path = Path(DATA_DIR, 'rule_based_input_output.json')
-    test_data: list[tuple[str, str, str]] = []
-    expected_usas_tags: list[list[str]] = []
+    test_data: List[Tuple[str, str, str]] = []
+    expected_usas_tags: List[List[str]] = []
     with test_data_path.open('r') as test_data_fp:
         for token_data in json.load(test_data_fp):
             token = token_data['token']
